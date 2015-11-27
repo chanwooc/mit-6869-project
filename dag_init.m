1652ecb1aec85c0fff36ee210b5030650853bb38
Joonwon Choi <joonwonc@mit.edu>
2015-11-26 20:14:43 -0500

    object_annot executable added; but has some bugs

--- dag_init.m
+++
@@ -1,8 +0,0 @@
-function [net] = dag_init(varargin)
-
-  % Bring configuration from simplenn
-  % net = miniplace_init(varargin);    
-  net = miniplace_init();
-  netDag = dagnn.DagNN.fromSimpleNN(net);
-
-end
